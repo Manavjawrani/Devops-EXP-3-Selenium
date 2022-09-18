19f6910b2d06738393b46b773970541c737d96c9
Manav <manavjawrani7@gmail.com>
2022-09-18 22:30:24 +0530

    Update

--- src/main/java/com/base/BaseClass.java
+++ src/main/java/com/base/BaseClass.java
@@ -6,12 +6,9 @@ public class BaseClass {
 
     public static void main(String[] args) {
         // declaration and instantiation of objects/variables
-    	System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+    	System.setProperty("webdriver.chrome.driver","E:\Devops EXP 3 Selenium\lib\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		//comment the above 2 lines and uncomment below 2 lines to use Chrome
-		//System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
-		//WebDriver driver = new ChromeDriver();
-    	
+		
         String baseUrl = "http://demo.guru99.com/test/newtours/";
         String expectedTitle = "Welcome: Mercury Tours";
         String actualTitle = "";
